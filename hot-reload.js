2435a15c8821da770aebb3a94b288a28b966d0ee
Yuxiang Yang <yuxiang.yang@shanbay.com>
2020-12-29 15:34:23 +0800

    refactor: refine hot reloading

--- hot-reload.js
+++ hot-reload.js
@@ -1,3 +1,4 @@
+// https://60devs.com/hot-reloading-for-chrome-extensions.html
 const filesInDirectory = dir =>
   new Promise(resolve =>
     dir.createReader().readEntries(entries =>
@@ -10,14 +11,14 @@ const filesInDirectory = dir =>
               : new Promise(resolve => e.file(resolve))
           )
       )
-        .then(files => [].concat(...files))
+        .then(files => [...files])
         .then(resolve)
     )
   );
 
 const timestampForFilesInDirectory = dir =>
   filesInDirectory(dir).then(files =>
-    files.map(f => f.name + f.lastModifiedDate).join()
+    files.map(f => f.name + f.lastModified).join()
   );
 
 const watchChanges = (dir, lastTimestamp) => {
@@ -32,7 +33,7 @@ const watchChanges = (dir, lastTimestamp) => {
 
 chrome.management.getSelf(self => {
   if (self.installType === 'development') {
-    chrome.runtime.getPackageDirectoryEntry(dir => watchChanges(dir));
+    chrome.runtime.getPackageDirectoryEntry(watchChanges);
     chrome.tabs.query({ active: true, lastFocusedWindow: true }, tabs => {
       // NB: see https://github.com/xpl/crx-hotreload/issues/5
       if (tabs[0]) {
